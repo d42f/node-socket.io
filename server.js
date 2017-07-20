5def68ad5b936894aa614ddd5bf214723216687d
Dmitri Frolov <dmitri.frolof@gmail.com>
2017-07-20 17:08:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,5 +17,3 @@ io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
-
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
