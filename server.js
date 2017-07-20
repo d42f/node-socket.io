3239600c6564c40028767a290d7d08f2c1aa1a0a
Dmitri Frolov <dmitri.frolof@gmail.com>
2017-07-20 17:44:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -16,4 +16,5 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('broadcast', (data) => io.emit('broadcast', data));
 });
